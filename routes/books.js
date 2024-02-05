4a40ecaaeda36e4d25210343aa602c41f8759c6c
“j-t-thomas” <janethomas1296@gmail.com>
2024-02-05 12:33:52 -0800

    Fixed update books

--- routes/books.js
+++ routes/books.js
@@ -22,12 +22,12 @@ router.post('/new', async function (req, res, next) {
   try {
     // Attempt to create a new book with data from the request body
     book = await Book.create(req.body);
-    res.redirect('/books/');
+    res.redirect('/books/' );
   } catch (error) {
     // If there are validation errors, re-render 'new-book' with the errors
     if (error.name === 'SequelizeValidationError') {
       book = await Book.build(req.body);
-      res.render('new-book', { book, errors: error.errors });
+      res.render('form', { book, errors: error.errors, });
     } else {
       throw error; // Propagate other errors
     }
@@ -35,24 +35,25 @@ router.post('/new', async function (req, res, next) {
 });
 
 // Render the 'update-book' view for editing a specific book
-router.get('/books/:id', async function (req, res, next) {
+router.get('/:id', async function (req, res, next) {
   const book = await Book.findByPk(req.params.id);
-  if (book) {
-    res.render('update-book', { book: book });
+  if (book === null) {
+    res.status(404); 
+    res.render('page-not-found');
   } else {
-    res.sendStatus(404); // Book not found
+    res.render('update-book', { book });
   }
 });
 
 // Update a specific book, redirect to '/books/' on success, or re-render 'update-book' with errors on failure
-router.post('/books/:id', async function (req, res, next) {
+router.post('/:id', async function (req, res, next) {
   let book;
   try {
     // Find the book by its ID and update its data
     book = await Book.findByPk(req.params.id);
     if (book) {
       await book.update(req.body);
-      res.redirect('/books/');
+      res.redirect('/books/' + book.id);
     } else {
       res.sendStatus(404); // Book not found
     }
@@ -61,7 +62,7 @@ router.post('/books/:id', async function (req, res, next) {
     if (error.name === 'SequelizeValidationError') {
       book = await Book.build(req.body);
       book.id = req.params.id;
-      res.render('update-book', { book, errors: error.errors });
+      res.render('form', { book, errors: error.errors });
     } else {
       throw error; // Propagate other errors
     }
@@ -73,7 +74,7 @@ router.post('/:id/delete', async function (req, res, next) {
   const book = await Book.findByPk(req.params.id);
   if (book) {
     await book.destroy();
-    res.redirect('/books');
+    res.redirect('/');
   } else {
     res.sendStatus(404); // Book not found
   }
